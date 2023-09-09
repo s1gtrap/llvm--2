7f564c26a46659e558ec3427ef0a1302233a8cd9
William Tange <will@tan.ge>
2023-09-09 10:38:44 +0200

    Test from_string prog

--- lib/parse.ml
+++ lib/parse.ml
@@ -98,3 +98,92 @@ let%test "prog1" =
             } );
         ];
     }
+
+let%test "prog2" =
+  from_string Llparser.prog
+    "@errmsg = global [19 x i8] c\"invalid arguments\\0A\\00\"\n\
+     declare i32 @puts(i8*)\n\
+     define i32 @main(i32 %argc, i8** %argv) {\n\
+    \    %cnd = icmp ne i32 %argc, 2\n\
+    \    br i1 %cnd, label %exit, label %succ\n\
+     exit:\n\
+    \    call i32 @puts(i8* @errmsg)\n\
+    \    ret i32 1\n\
+     succ:\n\
+    \    %arg1str = load i8*, i8** %arg1ptr\n\
+    \    call i32 @puts(i8* %arg1str)\n\
+    \    ret i32 0\n\
+     }"
+  = {
+      tdecls = [];
+      extgdecls = [];
+      gdecls =
+        [
+          (* @errmsg = global [19 x i8] c"invalid arguments\\0A\\00" *)
+          ( Symbol.symbol "errmsg",
+            (Array (19, I8), GString "invalid arguments\x0A\x00") );
+        ];
+      extfuns = [];
+      fdecls =
+        [
+          (* define i32 @main(i32 %argc, i8** %argv) { *)
+          ( Symbol.symbol "main",
+            {
+              fty = ([ I32; Ptr (Ptr I8) ], I32);
+              param = [ Symbol.symbol "argc"; Symbol.symbol "argv" ];
+              cfg =
+                ( {
+                    insns =
+                      [
+                        (* %cnd = icmp ne i32 %argc, 2 *)
+                        ( Some (Symbol.symbol "cnd"),
+                          Icmp (Ne, I32, Id (Symbol.symbol "argc"), IConst64 2L)
+                        );
+                      ];
+                    terminator =
+                      (* br i1 %cnd0, label %exit, label %succ *)
+                      Cbr
+                        ( Id (Symbol.symbol "cnd"),
+                          Symbol.symbol "exit",
+                          Symbol.symbol "succ" );
+                  },
+                  [
+                    (* exit: *)
+                    ( Symbol.symbol "exit",
+                      {
+                        insns =
+                          [
+                            (* call i32 @puts(i8* @errmsg) *)
+                            ( None,
+                              Call
+                                ( I32,
+                                  Gid (Symbol.symbol "puts"),
+                                  [ (Ptr I8, Gid (Symbol.symbol "errmsg")) ] )
+                            );
+                          ];
+                        terminator = (* ret i32 1 *)
+                                     Ret (I32, Some (IConst64 1L));
+                      } );
+                    (* succ: *)
+                    ( Symbol.symbol "succ",
+                      {
+                        insns =
+                          [
+                            (* %arg1str = load i8*, i8** %arg1ptr *)
+                            ( Some (Symbol.symbol "arg1str"),
+                              Load (Ptr I8, Id (Symbol.symbol "arg1ptr")) );
+                            (* call i32 @puts(i8* %arg1str) *)
+                            ( None,
+                              Call
+                                ( I32,
+                                  Gid (Symbol.symbol "puts"),
+                                  [ (Ptr I8, Id (Symbol.symbol "arg1str")) ] )
+                            );
+                          ];
+                        terminator = (* ret i32 0 *)
+                                     Ret (I32, Some (IConst64 0L));
+                      } );
+                  ] );
+            } );
+        ];
+    }
