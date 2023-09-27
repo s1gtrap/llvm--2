ed29410fcb0b911841dc325a59e4b397b5075261
William Tange <will@tan.ge>
2023-09-27 11:02:49 +0200

    Some debug tools for S.table

--- lib/lva.ml
+++ lib/lva.ml
@@ -3,5 +3,10 @@ module S = Symbol
 type lva = (int * int) S.table
 
 let lva_of_cfg (_ : Ll.cfg) : lva = S.empty
-let%test "lva_of_cfg" = List.equal Int.equal (List.rev [ 3; 2; 1 ]) [ 1; 2; 3 ]
+
+let%test "lva_of_cfg" =
+  let blk : Ll.block = { insns = []; terminator = Ret (Void, None) } in
+  let cfg : Ll.cfg = (blk, []) in
+  S.equal (lva_of_cfg cfg, S.table_of_list [])
+
 let%test "rev" = List.equal Int.equal (List.rev [ 3; 2; 1 ]) [ 1; 2; 3 ]
--- lib/symbol.ml
+++ lib/symbol.ml
@@ -34,14 +34,35 @@ let enter (t, k, v) = ST.add k v t
 let look (t, k) = ST.find_opt k t
 let numItems = ST.cardinal
 
+let string_of_table (f : 'a -> string) (t : 'a table) =
+  let entry k v s = Printf.sprintf "%s\n%s: %s" s (name k) (f v) in
+  ST.fold entry t ""
+
+let print_pair (a, b) = Printf.sprintf "(%d, %d)" a b
+let equal (a, b) = ST.equal (fun a b -> a = b) a b
+
 let table_of_list (entries : (string * (int * int)) list) : (int * int) table =
   let enter lva (k, v) = enter (lva, k, v) in
   let symbol (s, (k, v)) = (symbol s, (k, v)) in
   let entries = List.map symbol entries in
   List.fold_left enter empty entries
 
-let%test "table_of_list" =
-  let eq a b = a == b in
-  let teq (a, b) = ST.equal eq a b in
-  List.for_all teq
-    [ (enter (empty, symbol "a", (1, 2)), table_of_list [ ("a", (1, 2)) ]) ]
+let%test "equal_empty" = equal (empty, empty)
+
+let%test "equal_single" =
+  equal (enter (empty, symbol "a", (2, 3)), enter (empty, symbol "a", (2, 3)))
+
+let%test "not_equal_single" =
+  not
+    (equal
+       (enter (empty, symbol "b", (3, 4)), enter (empty, symbol "c", (4, 5))))
+
+let%test "table_of_list_empty" = equal (table_of_list [], empty)
+
+let%test "table_of_list_single" =
+  equal (table_of_list [ ("d", (5, 6)) ], enter (empty, symbol "d", (5, 6)))
+
+let%test "table_of_list_single2" =
+  equal (table_of_list [ ("d", (5, 6)) ], table_of_list [ ("d", (5, 6)) ])
+
+(*[ (enter (empty, symbol "a", (1, 2)), table_of_list [ ("a", (1, 2)) ]) ]*)
