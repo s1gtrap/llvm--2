63a5acad7a167090d0a01b4405d3f15ec75537ea
William Tange <will@tan.ge>
2023-10-02 13:01:41 +0200

    Match cases other than (Id _, Id _)

--- lib/lva.ml
+++ lib/lva.ml
@@ -2,9 +2,18 @@ module S = Symbol
 
 type lva = (int * int) S.table
 
-let lva_of_insn (l : lva) (idx : int) = function
-  | Some d, Ll.Binop _ -> (S.enter (l, d, (idx + 1, -1)), idx + 1)
-  | _ -> failwith ""
+let lva_of_insn (l : lva) (idx : int) insn =
+  let def l = function Some d -> S.enter (l, d, (idx + 1, -1)) | None -> l in
+  let use l = function
+    | Ll.Id s -> S.enter (l, s, (fst (Option.get (S.look (l, s))), idx + 1))
+    | _ -> l
+  in
+  let l =
+    match insn with
+    | d, Ll.Binop (_, _, a, b) -> ((def l d |> use) a |> use) b
+    | _ -> failwith ""
+  in
+  (l, idx + 1)
 
 let%test "lva_of_insn a = 1 + 2" =
   let insn =
@@ -74,4 +83,16 @@ let%test "lva_of_cfg { [a = 1 + 2], Ret a }" =
   let cfg : Ll.cfg = (blk, []) in
   S.equal (lva_of_cfg cfg, S.table_of_list [ ("a", (1, 2)) ])
 
-let%test "rev" = List.equal Int.equal (List.rev [ 3; 2; 1 ]) [ 1; 2; 3 ]
+let%test "lva_of_cfg { [a = 1 + 2; b = a + 3], Ret b }" =
+  let blk : Ll.block =
+    {
+      insns =
+        [
+          (Some (S.symbol "a"), Binop (Add, I32, IConst32 1l, IConst32 2l));
+          (Some (S.symbol "b"), Binop (Add, I32, Id (S.symbol "a"), IConst32 3l));
+        ];
+      terminator = Ret (I32, Some (Id (S.symbol "b")));
+    }
+  in
+  let cfg : Ll.cfg = (blk, []) in
+  S.equal (lva_of_cfg cfg, S.table_of_list [ ("a", (1, 2)); ("b", (2, 3)) ])
