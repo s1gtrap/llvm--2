33f336f540a744f24d3f1b8cb1a993b9ec99d0dc
William Tange <will@tan.ge>
2023-10-02 10:23:42 +0200

    Return offset from all lva_*

--- lib/lva.ml
+++ lib/lva.ml
@@ -3,41 +3,43 @@ module S = Symbol
 type lva = (int * int) S.table
 
 let lva_of_insn (l : lva) (idx : int) = function
-  | Some d, Ll.Binop _ -> S.enter (l, d, (idx + 1, -1))
+  | Some d, Ll.Binop _ -> (S.enter (l, d, (idx + 1, -1)), idx + 1)
   | _ -> failwith ""
 
 let%test "lva_of_insn a = 1 + 2" =
   let insn =
     (Some (S.symbol "a"), Ll.Binop (Add, I32, IConst32 1l, IConst32 2l))
   in
-  S.equal (lva_of_insn S.empty 0 insn, S.table_of_list [ ("a", (1, -1)) ])
+  let l, o = lva_of_insn S.empty 0 insn in
+  S.equal (l, S.table_of_list [ ("a", (1, -1)) ]) && o == 1
 
 let%test "lva_of_insn a = 1 + 2" =
   let insn =
     (Some (S.symbol "a"), Ll.Binop (Add, I32, IConst32 1l, IConst32 2l))
   in
-  S.equal (lva_of_insn S.empty 0 insn, S.table_of_list [ ("a", (1, -1)) ])
+  let l, o = lva_of_insn S.empty 0 insn in
+  S.equal (l, S.table_of_list [ ("a", (1, -1)) ]) && o == 1
 
 let lva_of_term (l : lva) (idx : int) = function
-  | Ll.Ret (_, None) -> l
+  | Ll.Ret (_, None) -> (l, idx + 1)
   | Ll.Ret (_, Some (Id s)) ->
-      S.enter (l, s, (fst (Option.get (S.look (l, s))), idx + 1))
+      (S.enter (l, s, (fst (Option.get (S.look (l, s))), idx + 1)), idx + 1)
   | _ -> failwith ""
 
 let%test "lva_of_term Ret a" =
   let term = Ll.Ret (I32, Some (Id (S.symbol "a"))) in
-  S.equal
-    ( lva_of_term (S.table_of_list [ ("a", (1, -1)) ]) 1 term,
-      S.table_of_list [ ("a", (1, 2)) ] )
+  let l, o = lva_of_term (S.table_of_list [ ("a", (1, -1)) ]) 1 term in
+  S.equal (l, S.table_of_list [ ("a", (1, 2)) ]) && o == 2
 
-let lva_of_block (l : lva) ({ insns; terminator } : Ll.block) : lva =
-  let fold (l, idx) insn = (lva_of_insn l idx insn, idx + 1) in
-  let l, i = List.fold_left fold (l, 0) insns in
+let lva_of_block (l : lva) idx ({ insns; terminator } : Ll.block) : lva * int =
+  let fold (l, idx) insn = lva_of_insn l idx insn in
+  let l, i = List.fold_left fold (l, idx) insns in
   lva_of_term l i terminator
 
 let%test "lva_of_block { [], Ret }" =
   let blk : Ll.block = { insns = []; terminator = Ret (Void, None) } in
-  S.equal (lva_of_block S.empty blk, S.table_of_list [])
+  let l, o = lva_of_block S.empty 0 blk in
+  S.equal (l, S.table_of_list []) && o == 1
 
 let%test "lva_of_block { [a = 1 + 2], Ret a }" =
   let blk : Ll.block =
@@ -47,9 +49,14 @@ let%test "lva_of_block { [a = 1 + 2], Ret a }" =
       terminator = Ret (I32, Some (Id (S.symbol "a")));
     }
   in
-  S.equal (lva_of_block S.empty blk, S.table_of_list [ ("a", (1, 2)) ])
+  let l, o = lva_of_block S.empty 0 blk in
+  S.equal (l, S.table_of_list [ ("a", (1, 2)) ]) && o == 2
 
-let lva_of_cfg (_ : Ll.cfg) : lva = S.empty
+let lva_of_cfg ((head, tail) : Ll.cfg) : lva =
+  let l, o = lva_of_block S.empty 0 head in
+  let fold (l, o) (_, blk) = lva_of_block l o blk in
+  let l, _ = List.fold_left fold (l, o) tail in
+  l
 
 let%test "lva_of_cfg { [], Ret }" =
   let blk : Ll.block = { insns = []; terminator = Ret (Void, None) } in
