12d85e2518ffd68158d3d4a5add575d69814354b
William Tange <will@tan.ge>
2023-10-30 16:54:36 +0100

    for some reason the alarm is being ignored!?

--- bin/testrunner.ml
+++ bin/testrunner.ml
@@ -21,42 +21,62 @@ let execute command : status =
   match Unix.fork () with
   | 0 -> (
       let _ = Unix.alarm 2 in
+      Printf.printf "spawning child...\n";
+      flush stdout;
       let pid =
         Unix.create_process_env command [| command |] (Unix.environment ())
           Unix.stdin stdout_write Unix.stderr
       in
+      Printf.printf "spawned child!\n";
+      flush stdout;
       match Unix.waitpid [] pid with
-      | _, WEXITED code -> exit code
+      | _, WEXITED code ->
+          Unix.close stdout_read;
+          Printf.printf "exiting WEXITED %d\n" code;
+          flush stdout;
+          exit code
       | _, WSIGNALED code ->
-          Printf.eprintf "WSIGNALED %d\n" code;
+          Printf.printf "exiting WSIGNALED %d\n" code;
+          flush stdout;
+          Unix.close stdout_read;
           exit 0xffffffff
       | _, WSTOPPED code ->
-          Printf.eprintf "WSTOPPED %d\n" code;
+          Printf.printf "exiting WSTOPPED %d\n" code;
+          flush stdout;
+          Unix.close stdout_read;
           exit 0xffffffff)
   | pid ->
-      let read read write =
-        Unix.close write;
-        let output = ref "" in
-        let buffer = Bytes.create 1024 in
-        let rec r (_ : unit) =
-          let len = Unix.read stdout_read buffer 0 1024 in
-          if len > 0 then (
-            output := !output ^ Bytes.sub_string buffer 0 len;
-            r ())
-        in
-        r ();
-        Unix.close read;
-        !output
+      Unix.close stdout_write;
+      let output = ref "" in
+      let buffer = Bytes.create 1024 in
+      let rec read_output () =
+        flush stdout;
+        let len = Unix.read stdout_read buffer 0 1024 in
+        flush stdout;
+        if len > 0 then (
+          output := !output ^ Bytes.sub_string buffer 0 len;
+          read_output ())
       in
-      let stdout = try read stdout_read stdout_write with _ -> "" in
+      Printf.printf "reading stdout...\n";
+      flush stdout;
+      (try read_output () with _ -> ());
+      Unix.close stdout_read;
+      Printf.printf "done reading stdout!\n";
+      flush stdout;
 
+      Printf.printf "waiting...\n";
+      flush stdout;
       let _ = Unix.waitpid [] pid in
-      Exit (0, stdout, "")
+      Printf.printf "done!\n";
+      flush stdout;
+      Exit (0, !output, "")
 
 let () =
   let _ = Printf.sprintf "%s\n" (string_of_status Timeout) in
   let _ =
     Printf.sprintf "%s\n" (string_of_status (Error (WSTOPPED 0, "", "")))
   in
+  Printf.printf "\n1:\n";
   Printf.printf "%s\n" (string_of_status (execute "ls"));
+  Printf.printf "\n2:\n";
   Printf.printf "%s\n" (string_of_status (execute "xxd"))
